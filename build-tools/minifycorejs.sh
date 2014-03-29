a9093d63dd31e6f3be6d7f119b939e9429b9c049
Hunter Perrin <hperrin@gmail.com>
2014-03-28 17:44:15 -0700

    Missed one.

--- build-tools/minifycorejs.sh
+++ build-tools/minifycorejs.sh
@@ -2,6 +2,6 @@
 
 echo Compressing JavaScript with Google Closure Compiler.
 head -n 8 "../../2be/system/includes/core.js" > "../../2be/system/includes/core.min.js"
-java -jar compiler.jar --js="../../2be/system/includes/core.js" >> "../../pines/system/includes/core.min.js"
+java -jar compiler.jar --js="../../2be/system/includes/core.js" >> "../../2be/system/includes/core.min.js"
 
 echo Done.
